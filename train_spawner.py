e8863b0ee48cddd936fac442abf015e9e1b7d4ba
kaca <kacabartosz2@gmail.com>
2023-04-28 12:10:43 +0200

    train spawner cd

--- train_spawner.py
+++ train_spawner.py
@@ -6,4 +6,10 @@ def train_generator():
     for x in range(random(0,4)):
         for y in x:
             index = random(0,3)
-
+            match index:
+                case 0:
+                    train = Train(position=(-20, 0, 100), scale=(10, 10, 20))
+                case 1:
+                    train = Train(position=(0, 0, 100), scale=(10, 10, 20))
+                case 2:
+                    train = Train(position=(20, 0, 100), scale=(10, 10, 20))
