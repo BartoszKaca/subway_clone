451fc31e651dd302bada631cce358b08514ffb38
kaca <kacabartosz2@gmail.com>
2023-04-28 09:32:18 +0200

    train spawner init

--- train_spawner.py
+++ train_spawner.py
@@ -1,2 +1,9 @@
 from Train import *
+from random import *
+from GameParameters import *
+tablica = [0,0,0]
+def train_generator():
+    for x in range(random(0,4)):
+        for y in x:
+            index = random(0,3)
 
