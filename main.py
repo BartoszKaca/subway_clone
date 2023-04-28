13fbe6888553ac8990449640bfcf68642366f61b
kaca <kacabartosz2@gmail.com>
2023-04-28 12:43:33 +0200

    train spawner first version finished

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from Player import *
 from Train import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.prefabs.editor_camera import *
+from train_spawner import *
 
 app = Ursina()
 player = Player(collider = 'box',model = 'cube', position = (0, 0, 0))
 #player = FirstPersonController(collider = 'box',model = 'cube', position = (0, 0, 0))
 ground = Entity(model = 'plane', texture = 'tracks.png', collider = 'mesh', scale = (60, 1, 100), position = (0,0,50))
-train = Train(position = (0, 0, 100), scale = (10,10,20))
+train = train_generator()
 #plus = Entity(model = 'cube', texture = 'brick', scale = (10,10,20), collider = 'box', position = (0,0,0))
 Sky()
 
--- train_spawner.py
+++ train_spawner.py
@@ -3,9 +3,8 @@ from random import *
 from GameParameters import *
 tablica = [0,0,0]
 def train_generator():
-    for x in range(random(0,4)):
-        for y in x:
-            index = random(0,3)
+    for x in range(random.randrange(0,4)):
+            index = random.randrange(0,3)
             match index:
                 case 0:
                     train = Train(position=(-20, 0, 100), scale=(10, 10, 20))
@@ -13,3 +12,4 @@ def train_generator():
                     train = Train(position=(0, 0, 100), scale=(10, 10, 20))
                 case 2:
                     train = Train(position=(20, 0, 100), scale=(10, 10, 20))
+    return train
