17a05d21719351ec975d64c67cd3e07677cc78f4
kaca <kacabartosz2@gmail.com>
2023-05-06 20:20:28 +0200

    suicide

--- Menu.py
+++ Menu.py
@@ -8,17 +8,25 @@ class Menu(Entity):
         self.help_menu = Entity(parent = self)
         self.background = Sprite('menu_background.png', z = -8.9,y =-1, rotation = (180,0,0))
         player.rotation = (180,0,0)
-        buttons = {
-            'start': lambda : Func(self.close_menu(player)),
-            'jak to sie kurwa dzieje': lambda :Func(self.kontrole(player)),
-            'wyjdz': lambda : application.quit()
+        self.buttons = {
+            'start':Button(text= 'start', scale = (.4,.1), position = (-.65, .15),color = color.black),
+            'sterowanie':Button( text='sterowanie', scale = (.4,.1), position = (-.65,0),color=color.black),
+            'wyjdź':Button(text='wyjdź', scale = (.4,.1), position = (-.65,-.15), color=color.black)
         }
-        self.bl = ButtonList(buttons, position = (-0.85,0.5), button_height=5, width = .5)
+        self.buttons['start'].on_click=lambda:Func(self.close_menu(player))
+        self.buttons['sterowanie'].on_click=lambda :Func(self.kontrole(player))
+        self.buttons['wyjdź'].on_click=lambda:application.quit()
 
     def close_menu(self, player):
         player.rotation=(0,0,0)
         GameParameters.paused = False
-        self.bl.disable()
+        for key in self.buttons.keys():
+            self.buttons[key].disable()
     def kontrole(self,player):
-        player.rotation=(0,0,0)
-        GameParameters.paused = False
+        for key in self.buttons.keys():
+            self.buttons[key].disable()
+    def show_menu(self, player):
+        player.rotation=(180,0,0)
+        GameParameters.paused = True
+        for key in self.buttons.keys():
+            self.buttons[key].enable()
